91234e52a7510c8d46e8472883e7b12bcdefb72b
DevBees <82749068+DevBees@users.noreply.github.com>
2022-11-01 14:32:02 +0530

    Update cart_screen.dart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -8,76 +8,78 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue.shade900,
-        title: Text('Total Cart Items : '
-            // '${context.watch<ShopList>().cartCount}'
-            ),
-        centerTitle: true,
-      ),
-      body: context.watch<ShopList>().cartCount == 0
-          ? Center(
-              child: Text(
-                "No Items in cart",
-                style: TextStyle(
-                  color: Colors.blueGrey.shade400.withOpacity(0.5),
-                  fontSize: 40,
-                  fontWeight: FontWeight.w600,
-                ),
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          title: Text('Total Cart Items : '
+              // '${context.watch<ShopList>().cartCount}'
               ),
-            )
-          : ListView.separated(
-              separatorBuilder: (_, index) => Divider(
-                    thickness: 2,
-                  ),
-              itemCount: 1, //replace with cart item count
-              // context.watch<ShopList>().cartItemCount,
-              itemBuilder: (_, index) {
-                return ListTile(
-                  leading: Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.red //change to value from items list
-                      // context.read<ShopList>().cartItems[index].color,
-                      ),
-                  //cart list item name
-                  title: Text(''
-                      // context.read<ShopList>().cartItems[index].name,
-                      ),
-                  trailing: Container(
-                    width: 110,
-                    child: Row(
-                      children: [
-                        IconButton(
-                            color: Colors.blue.shade900,
-                            onPressed: () {
-                              // context.read<ShopList>().addToCart(
-                              //     context.read<ShopList>().cartItems[index]);
-                              // print(context.read<ShopList>().cartItems);
-                            },
-                            icon: Icon(Icons.add_circle)),
-                        Text(
-                          ''
-                          // '${context.watch<ShopList>().cartItems[index].count}',
-                          ,
-                          style: TextStyle(
-                            fontSize: 18,
-                          ),
-                        ),
-                        IconButton(
-                            disabledColor: Colors.blueGrey.shade500,
-                            color: Colors.blue.shade900,
-                            onPressed: () {
-                              // context.read<ShopList>().removeFromCart(
-                              //     context.read<ShopList>().cartItems[index]);
-                              // print(context.read<ShopList>().cartItems);
-                            },
-                            icon: Icon(Icons.remove_circle)),
-                      ],
-                    ),
-                  ),
-                );
-              }),
-    );
+          centerTitle: true,
+        ),
+        body:
+            // context.watch<ShopList>().cartCount == 0
+            //     ?
+            Center(
+          child: Text(
+            "No Items in cart",
+            style: TextStyle(
+              color: Colors.blueGrey.shade400.withOpacity(0.5),
+              fontSize: 40,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        )
+        // : ListView.separated(
+        //     separatorBuilder: (_, index) => Divider(
+        //           thickness: 2,
+        //         ),
+        //     itemCount: 1, //replace with cart item count
+        //     // context.watch<ShopList>().cartItemCount,
+        //     itemBuilder: (_, index) {
+        //       return ListTile(
+        //         leading: Container(
+        //             width: 40,
+        //             height: 40,
+        //             color: Colors.red //change to value from items list
+        //             // context.read<ShopList>().cartItems[index].color,
+        //             ),
+        //         //cart list item name
+        //         title: Text(''
+        //             // context.read<ShopList>().cartItems[index].name,
+        //             ),
+        //         trailing: Container(
+        //           width: 110,
+        //           child: Row(
+        //             children: [
+        //               IconButton(
+        //                   color: Colors.blue.shade900,
+        //                   onPressed: () {
+        //                     // context.read<ShopList>().addToCart(
+        //                     //     context.read<ShopList>().cartItems[index]);
+        //                     // print(context.read<ShopList>().cartItems);
+        //                   },
+        //                   icon: Icon(Icons.add_circle)),
+        //               Text(
+        //                 ''
+        //                 // '${context.watch<ShopList>().cartItems[index].count}',
+        //                 ,
+        //                 style: TextStyle(
+        //                   fontSize: 18,
+        //                 ),
+        //               ),
+        //               IconButton(
+        //                   disabledColor: Colors.blueGrey.shade500,
+        //                   color: Colors.blue.shade900,
+        //                   onPressed: () {
+        //                     // context.read<ShopList>().removeFromCart(
+        //                     //     context.read<ShopList>().cartItems[index]);
+        //                     // print(context.read<ShopList>().cartItems);
+        //                   },
+        //                   icon: Icon(Icons.remove_circle)),
+        //             ],
+        //           ),
+        //         ),
+        //       );
+        //     }),
+        );
   }
 }
