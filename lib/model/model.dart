124da5ec95840cb3c1aa3f250f79220eee5db805
DevBees <82749068+DevBees@users.noreply.github.com>
2022-11-01 14:07:53 +0530

    Add files via upload

---
+++ lib/model/model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Item {
+  final String name;
+  final Color color;
+  int count = 0;
+  Item({required this.name, required this.color, required this.count});
+}
